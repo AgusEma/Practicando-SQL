38617c532910a82ef28e6ea5808eda6c535f9c58
AgusEma <agusallocco12@gmail.com>
2022-08-28 09:40:32 -0300

    usando insert y select

--- practicando.sql
+++ practicando.sql
@@ -9,6 +9,11 @@ create table carrera(
   constraint pk_cc primary key(clave_c)
 )
 
+insert into carrera values(1, 'Derecho', 3)
+insert into carrera values(2, 'Ingenieria en Sistemas', 4)
+
+select * from carrera
+
 --Tabla materia
 create table materia(
   clave_m int,
@@ -16,6 +21,11 @@ create table materia(
   cred_m float,
   constraint pk_cm primary key(clave_m)
 )
+
+insert into materia values(1, 'Matematicas', 25)
+insert into materia values(2, 'Espanol', 15)
+
+select clave_m, nom_m from materia
 
 --Tabla profesor
 create table profesor(
@@ -26,6 +36,9 @@ create table profesor(
   hor_p datetime,
   constraint pk_cp primary key(clave_p)
 )
+
+insert into profesor values(1, 'Laura Rosas', 'Calle Azul', 567867, '7:00')
+insert into profesor values(2, 'Roberto Diaz', 'Calle Rosa', 56743526, '9:00')
 
 --Tabla alumno
 create table alumno(
@@ -39,6 +52,8 @@ create table alumno(
   constraint fk_fc1 foreign key(clave_c1) references carrera(clave_c)
 )
 
+insert into alumno values (1, 'Ana', 18, 6, 'Femenino', 1)
+
 --Alumno-Profesor
 create table alu_pro(
   mat_alu1 int,
@@ -46,6 +61,8 @@ create table alu_pro(
   constraint fk_falu1 foreign key(mat_alu1) references alumno(mat_alu),
   constraint fk_fp1 foreign key(clave_p1) references profesor(clave_p)
 )
+
+insert into alu_pro values (1, 2)
 
 --Materia-Alumno
 create table mat_alu(
@@ -55,6 +72,8 @@ create table mat_alu(
   constraint fk_falu2 foreign key(mat_alu2) references alumno(mat_alu)
 )
 
+insert into mat_alu values(1, 1)
+
 --Materia-Profesor
 create table mat_pro(
   clave_m2 int,
@@ -62,3 +81,5 @@ create table mat_pro(
   constraint fk_fm1 foreign key(clave_m2) references materia(clave_m),
   constraint fk_fp2 foreign key(clave_p2) references profesor(clave_p)
 )
+
+insert into mat_pro values(2, 2)
